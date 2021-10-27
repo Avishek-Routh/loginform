b219f0e1fc4cb0c79f87111eef9f26d05d99b477
Avishek-Routh <91865540+Avishek-Routh@users.noreply.github.com>
2021-10-27 21:00:26 +0530

    Add files via upload

---
+++ data.php
@@ -0,0 +1,17 @@
+<?php
+$name=$_POST["name"];
+$mobile=$_POST["mobile"];
+$email=$_POST["email"];
+$city=$_POST["city"];
+$username=$_POST["username"];
+$password=$_POST["password"];
+
+echo "Name is: ".$name."<br>";
+echo "Mobile is: ".$mobile."<br>";
+echo "Email is: ".$email."<br>";
+echo "City is: ".$city."<br>";
+echo "Username is: ".$username."<br>";
+echo "Password is: ".$password."<br>";
+
+
+?>
